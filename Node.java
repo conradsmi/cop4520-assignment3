0d4362fae335f5d4bca72d0b7bb5c05a28eeec5c
Conrad Smith <conradsmith22@knights.ucf.edu>
2022-04-08 17:27:01 -0400

    added Node

---
+++ Node.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class Node<T> {
+    T item;
+    int key;
+    AtomicMarkableReference<Node<T>> next;
+
+    public Node(T item) {
+        this.item = item;
+        this.key = this.hashCode();
+        this.next = null;
+    }
+}
